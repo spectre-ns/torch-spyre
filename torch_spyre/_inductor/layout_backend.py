f0b159dbe866f67fbd7e9b80052bfb20aec9c19b
Drew Hubley <dahubley@hotmail.ca>
2026-04-23 15:23:43 -0300

    Added plotting back
    Signed-off-by: Drew Hubley <dahubley@hotmail.ca>

--- torch_spyre/_inductor/layout_backend.py
+++ torch_spyre/_inductor/layout_backend.py
@@ -3,6 +3,8 @@ import copy
 from itertools import combinations
 from typing import List, Tuple, Literal
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import patches
 
 
 @dataclass
@@ -42,88 +44,88 @@ class Operation:
         return ReadWrites(reads=reads, writes=writes)
 
 
-# def plot_layout(capacity: int, buffers: list[Buffer]):
-#     assert np.all([b.start_time is not None for b in buffers]), (
-#         "Start time must be defined"
-#     )
-#     assert np.all([b.end_time is not None for b in buffers]), "End time must be defined"
+def plot_layout(capacity: int, buffers: list[Buffer]):
+    assert np.all([b.start_time is not None for b in buffers]), (
+        "Start time must be defined"
+    )
+    assert np.all([b.end_time is not None for b in buffers]), "End time must be defined"
 
-#     fig, ax = plt.subplots(figsize=(10, 6))
-#     ax.axhline(
-#         y=capacity,
-#         color="red",
-#         linestyle="--",
-#         linewidth=2,
-#         label=f"SPM Ceiling ({capacity})",
-#     )
-#     ax.axhline(y=0, color="red", linestyle="--", linewidth=2, label="SPM Floor")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.axhline(
+        y=capacity,
+        color="red",
+        linestyle="--",
+        linewidth=2,
+        label=f"SPM Ceiling ({capacity})",
+    )
+    ax.axhline(y=0, color="red", linestyle="--", linewidth=2, label="SPM Floor")
 
-#     max_time = max([b.end_time for b in buffers if b.end_time is not None])
-#     ax.set_xlim(0, max_time + 1)
-#     ax.set_ylim(-capacity, capacity * 2)
-#     ax.set_xlabel("Time (Logical Steps)")
-#     ax.set_ylabel("Memory Address (Bytes)")
-#     ax.set_title("Calculated Memory Layout")
+    max_time = max([b.end_time for b in buffers if b.end_time is not None])
+    ax.set_xlim(0, max_time + 1)
+    ax.set_ylim(-capacity, capacity * 2)
+    ax.set_xlabel("Time (Logical Steps)")
+    ax.set_ylabel("Memory Address (Bytes)")
+    ax.set_title("Calculated Memory Layout")
 
-#     rects = {}
-#     texts = {}
+    rects = {}
+    texts = {}
 
-#     # Initialize the visual blocks
-#     for b in buffers:
-#         rect = patches.Rectangle(
-#             (b.start_time, 0),
-#             b.end_time - b.start_time,
-#             b.size,
-#             linewidth=1.5,
-#             edgecolor="black",
-#             facecolor="skyblue",
-#             alpha=0.8,
-#         )
-#         ax.add_patch(rect)
-#         rects[b.name] = rect
+    # Initialize the visual blocks
+    for b in buffers:
+        rect = patches.Rectangle(
+            (b.start_time, 0),
+            b.end_time - b.start_time,
+            b.size,
+            linewidth=1.5,
+            edgecolor="black",
+            facecolor="skyblue",
+            alpha=0.8,
+        )
+        ax.add_patch(rect)
+        rects[b.name] = rect
 
-#         txt = ax.text(
-#             0,
-#             0,
-#             f"Buffer {b.name}",
-#             ha="center",
-#             va="center",
-#             fontsize=10,
-#             weight="bold",
-#         )
-#         texts[b.name] = txt
+        txt = ax.text(
+            0,
+            0,
+            f"Buffer {b.name}",
+            ha="center",
+            va="center",
+            fontsize=10,
+            weight="bold",
+        )
+        texts[b.name] = txt
 
-#     step_text = ax.text(
-#         0.02, 0.90, "", transform=ax.transAxes, fontsize=12, weight="bold"
-#     )
-#     fig.canvas.draw()
-#     fig.canvas.flush_events()
+    step_text = ax.text(
+        0.02, 0.90, "", transform=ax.transAxes, fontsize=12, weight="bold"
+    )
+    fig.canvas.draw()
+    fig.canvas.flush_events()
 
-#     msg = "FINISHED! Showing Final Configuration"
-#     for b in buffers:
-#         if not b.spilled:
-#             rects[b.name].set_visible(True)
-#             texts[b.name].set_visible(True)
-#             rects[b.name].set_y(b.address)
-#             texts[b.name].set_position(
-#                 (
-#                     b.start_time + (b.end_time - b.start_time) / 2,
-#                     b.address + b.size / 2,
-#                 )
-#             )
-#         else:
-#             rects[b.name].set_visible(False)
-#             texts[b.name].set_visible(False)
+    msg = "FINISHED! Showing Final Configuration"
+    for b in buffers:
+        if not b.spilled:
+            rects[b.name].set_visible(True)
+            texts[b.name].set_visible(True)
+            rects[b.name].set_y(b.address)
+            texts[b.name].set_position(
+                (
+                    b.start_time + (b.end_time - b.start_time) / 2,
+                    b.address + b.size / 2,
+                )
+            )
+        else:
+            rects[b.name].set_visible(False)
+            texts[b.name].set_visible(False)
 
-#     # Add a warning box for spilled buffers
-#     spilled_buffers = [b.name + f" : {b.density:.2f}" for b in buffers if b.spilled]
-#     if spilled_buffers:
-#         msg += f"\nSpilled to DRAM: {', '.join(spilled_buffers)}"
-#     else:
-#         msg += "\n No evictions required"
+    # Add a warning box for spilled buffers
+    spilled_buffers = [b.name + f" : {b.density:.2f}" for b in buffers if b.spilled]
+    if spilled_buffers:
+        msg += f"\nSpilled to DRAM: {', '.join(spilled_buffers)}"
+    else:
+        msg += "\n No evictions required"
 
-#     step_text.set_text(msg)
-#     plt.show()
+    step_text.set_text(msg)
+    plt.show()
 
 
 # def calculate_logical_lifetimes(ops: list[Operation]) -> list[Buffer]:
@@ -397,7 +399,5 @@ if __name__ == "__main__":
         Buffer(name="C", size=800, start_time=3, end_time=5),
     ]
 
-    best_allocation = allocate_buffers(
-        SPM_CAPACITY, buffer_list, "ordered-global", allow_fallback=True
-    )
-    # plot_layout(SPM_CAPACITY, best_allocation)
+    best_allocation = allocate_buffers(SPM_CAPACITY, buffer_list, "ordered-global")
+    plot_layout(SPM_CAPACITY, best_allocation)
