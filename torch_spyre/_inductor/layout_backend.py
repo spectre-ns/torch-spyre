dffcb0553062ab4005ea63e7ac285047caf5abf3
Drew Hubley <dahubley@hotmail.ca>
2026-05-06 17:12:28 +0000

    Added greedy layout solver
    Signed-off-by: Drew Hubley <dahubley@hotmail.ca>

--- torch_spyre/_inductor/layout_backend.py
+++ torch_spyre/_inductor/layout_backend.py
@@ -16,6 +16,7 @@
 from dataclasses import dataclass
 from typing import list, Optional, dict, str, float
 from abc import ABC, abstractmethod
+import math
 
 
 @dataclass
@@ -57,3 +58,101 @@ class LayoutSolver(ABC):
             list[LifetimeBoundBuffer]: The set of buffers with their placements defined.
         """
         pass
+
+
+class GreedyLayoutSolver(LayoutSolver):
+    def __init__(self, size: int):
+        # scratch pad is 2MB = 2<<20 bytes in total. preserve total * DXP_LX_FRAC_AVAIL
+        # for backend usage unless specified otherwise
+        self.limit = size
+        self.usage: list[
+            LifetimeBoundBuffer
+        ] = []  # each record will be tensor_name:{"addr": yy, "size": zz}
+
+    def get_lowest_addr_in_use(self):
+        if len(self.usage) > 0:
+            return min([rec["addr"] for rec in self.usage.values()])
+        return None
+
+    def get_highest_addr_in_use(self):
+        if len(self.usage) > 0:
+            return max([rec["addr"] + rec["size"] for rec in self.usage.values()])
+        return None
+
+    def get_available_total(self):
+        total_avail = self.limit
+        for rec in self.usage.values():
+            total_avail -= rec["size"]
+        return total_avail
+
+    def find_free_block(self, size_needed: int) -> Optional[int]:
+        # cannot perform defragmentation yet, will add more cases in the future
+        curr_lo = self.get_lowest_addr_in_use()
+        curr_hi = self.get_highest_addr_in_use()
+        if len(self.usage) == 0 or curr_lo >= size_needed:
+            # completely free or enough room at addr0
+            return 0
+        elif curr_hi + size_needed < self.limit:
+            # enough room at higher addr, return next 128-multiple
+            return math.ceil(curr_hi / 128) * 128
+        elif len(self.usage) > 1:
+            # find a "hole" between lowest and highest (assume a block was dealloc'ed)
+            rec_only = list(self.usage.values())  # simply drop tensor names, not needed
+            sorted_rec = sorted(rec_only, key=lambda rec: rec["addr"])
+            for i in range(len(sorted_rec) - 1):
+                frag_st = sorted_rec[i]["addr"] + sorted_rec[i]["size"]
+                frag_end = sorted_rec[i + 1]["addr"]
+                if frag_end - frag_st >= size_needed:
+                    return frag_st
+            return None
+        else:
+            # cannot find any free blocks
+            return None
+
+    def try_allocate(self, buffer: LifetimeBoundBuffer):
+        """
+        Simple reuse rule:
+        1. for an "input" tensor, found a matched tensor (name and size) on LX
+        2. for an output tensor, if this op is on the "white list" => prep for pinning
+            => alloc a new LX block for the "output" of the op
+        If can_reuse => add lx info to corresponding buffer.layout
+        NOTE: 1. if an op, e.g. max, occurs multiple times on graph, output buffers will
+                 have different names -> end-of-life analysis will take care of dealloc
+              2. prev Op's sdsc.out.out.out.json may have useful info, not needed yet
+              3. may be able to generalize this decision in buf end-of-life analysis
+              4. greedy alloc may cause fragments, can further improve
+        """
+        # Decide whether to reuse.
+        addr = self.find_free_block(buffer.size)
+
+        if addr:
+            self.usage.append(buffer)
+            buffer.address = addr
+            buffer.spilled = False
+
+    def deallocate(self, bufs: list[str] | str):
+        """Try to deallocate each of the buffers in a list, if exists."""
+        if isinstance(bufs, str):
+            bufs = [bufs]
+
+        for buf in bufs:
+            if buf in self.usage:
+                del self.usage[buf]
+
+    def plan_layout(
+        self, buffers: list[LifetimeBoundBuffer]
+    ) -> list[LifetimeBoundBuffer]:
+        if not buffers:
+            return []
+
+        max_time = max(b.end_time for b in buffers)
+        for idx in range(max_time):
+            # attempt to allocate at based on time
+            for buffer in buffers:
+                if idx == buffer.start_time:
+                    self.try_allocate(buffer)
+
+                if idx == buffer.end_time:
+                    self.deallocate(buffer.name)
+
+        return buffers
