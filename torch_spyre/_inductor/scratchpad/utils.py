bf2ffc97364fb4cbe58dd99c40ee505a2509da9d
Drew Hubley <dahubley@hotmail.ca>
2026-05-19 11:39:01 +0000

    Passing lx planning tests with is_lx_viable flag
    Signed-off-by: Drew Hubley <dahubley@hotmail.ca>

--- torch_spyre/_inductor/scratchpad/utils.py
+++ torch_spyre/_inductor/scratchpad/utils.py
@@ -19,10 +19,10 @@ from torch._inductor.ir import Operation, ComputedBuffer
 from torch_spyre._inductor import config
 
 
-def calculate_liveness(
-    mem_usage: dict[str, dict[str, bool | int]], ops: Operation
+def calculate_liveness(graph: GraphLowering
 ) -> dict:
-    for i, op in enumerate(ops):
+    mem_usage: dict[str, dict[str, bool | int]] = {}
+    for i, op in enumerate(graph.operations):
         rw = op.get_read_writes()
         for mem_dep in rw.reads | rw.writes:
             buf_name = mem_dep.name
@@ -36,9 +36,8 @@ def calculate_liveness(
 
 def mem_usage_by_op(
     graph: GraphLowering,
-    ops: list[ComputedBuffer],
     core_div_mismatch: dict[str, bool] = {},
-) -> tuple[dict, dict]:
+) -> dict:
     """
     Get a summary of memory usage of the given operation. Two types of info can be found
     1. Name lists, e.g. mem_usage["all_inputs"], or "all_outputs", "all_buf_used"
@@ -49,7 +48,7 @@ def mem_usage_by_op(
     if a buf is on release_next => it's the last time it'll be used => allow inplace
     """
     mem_usage: dict = {}
-    for op in ops:
+    for op in graph.operations:
         rw = op.get_read_writes()
         mem_usage[op.name] = {
             "all_inputs": [],
@@ -62,16 +61,22 @@ def mem_usage_by_op(
         for is_input, deps in [(True, rw.reads), (False, rw.writes)]:
             for dep in deps:
                 buf = graph.get_buffer(dep.name)
-                dev_layout = buf.layout.device_layout
-                dev_size = (
-                    math.prod(dev_layout.device_size[:-1]) * 128
-                )  # num_sticks * bytes_per_stick
-                mem_usage[op.name][dep.name] = {
-                    "is_input": is_input,
-                    "size": dev_size,
-                    "size_per_core": dev_size // num_cores,
-                    "core_div_mismatch": core_div_mismatch.get(dep.name, False),
-                }
+                try:
+                    dev_layout = buf.layout.device_layout
+                    dev_size = (
+                        math.prod(dev_layout.device_size[:-1]) * 128
+                    )  # num_sticks * bytes_per_stick
+                    mem_usage[op.name][dep.name] = {
+                        "is_lx_viable" : True,
+                        "is_input": is_input,
+                        "size": dev_size,
+                        "size_per_core": dev_size // num_cores,
+                        "core_div_mismatch": core_div_mismatch.get(dep.name, False),
+                    }
+                except AttributeError:
+                    mem_usage[op.name][dep.name] = {
+                            "is_lx_viable" : False,
+                        }
 
                 if is_input:
                     mem_usage[op.name]["all_inputs"].append(dep.name)
@@ -80,10 +85,10 @@ def mem_usage_by_op(
         mem_usage[op.name]["all_buf_used"] = (
             mem_usage[op.name]["all_inputs"] + mem_usage[op.name]["all_outputs"]
         )
-    return mem_usage, calculate_liveness({}, ops)
+    return mem_usage
 
 
-def buf_analysis(operations: list[Operation]):
+def buf_analysis(graph: GraphLowering):
     """
     First, find out the last time each buffer was used. {buf1: idx_last_used, ...}
     Turn it into {idx_last_used+1:[buf1, ], ...}, ie. buffers to be deleted at given idx
@@ -101,7 +106,7 @@ def buf_analysis(operations: list[Operation]):
     buf_users_read_and_write: dict[str, list[Operation]] = {}
     core_div_mismatch: dict[str, bool] = {}
 
-    for idx, op in enumerate(operations):
+    for idx, op in enumerate(graph.operations):
         rw = op.get_read_writes()
         read_names = op.get_read_names()
         for dep in rw.reads | rw.writes:  # union of the OrderedSets
@@ -143,54 +148,3 @@ def get_buffer_users(graph: GraphLowering) -> dict[str, list[Operation]]:
             buf = dep.name  # buffer name, i.e. a str
             buf_users_read_and_write[buf] = buf_users_read_and_write.get(buf, []) + [op]
     return buf_users_read_and_write
-
-
-def get_ncores_for_buffers(graph: GraphLowering) -> dict[str, int]:
-    """
-    Return a dictionary mapping buffer names to the number of cores
-    used by all the operations that uses the buffer.
-    If there is a core division mismatch return -1 instead of the
-    number of cores.
-    """
-    result: dict[str, int] = {}
-    using_multicore = config.sencores > 1
-    buf_users_read_and_write = get_buffer_users(graph)
-    for buf_name, users_rw in buf_users_read_and_write.items():
-        # this dict includes graph input and output
-        if using_multicore:
-            # graph input and output can have only 1 read or 1 write user.
-            u0_split = users_rw[0].op_it_space_splits  # a list like [16, 1]
-            same_core_div = all(u0_split == u.op_it_space_splits for u in users_rw[1:])
-            num_cores = (
-                math.prod([s for p in u0_split for s in p.values()])
-                if same_core_div
-                else -1
-            )
-        else:
-            num_cores = 1
-        result[buf_name] = num_cores
-    return result
-
-
-def calculate_buffer_statistics(graph: GraphLowering) -> dict[str, dict[str, int]]:
-    buf_read_counts: dict[str, int] = {}
-    buf_write_counts: dict[str, int] = {}
-
-    for op in graph.operations:
-        rw = op.get_read_writes()
-        read_names = op.get_read_names()
-        for dep in rw.reads | rw.writes:
-            buf = dep.name
-            if buf in read_names:
-                buf_read_counts[buf] = buf_read_counts.get(buf, 0) + 1
-            else:
-                buf_write_counts[buf] = buf_write_counts.get(buf, 0) + 1
-
-    all_bufs = buf_read_counts.keys() | buf_write_counts.keys()
-    return {
-        buf: {
-            "reads": buf_read_counts.get(buf, 0),
-            "writes": buf_write_counts.get(buf, 0),
-        }
-        for buf in all_bufs
-    }
