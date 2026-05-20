9c6c31c7b4b146807098db3b891f29f7ab410722
Drew Hubley <dahubley@hotmail.ca>
2026-05-20 11:31:43 +0000

    Added graph view
    Signed-off-by: Drew Hubley <dahubley@hotmail.ca>

--- torch_spyre/_inductor/scratchpad/utils.py
+++ torch_spyre/_inductor/scratchpad/utils.py
@@ -17,6 +17,7 @@ import math
 from torch._inductor.graph import GraphLowering
 from torch._inductor.ir import Operation
 from torch_spyre._inductor import config
+from typing import Callable
 
 
 def calculate_liveness(graph: GraphLowering) -> dict:
@@ -33,10 +34,7 @@ def calculate_liveness(graph: GraphLowering) -> dict:
     return liveness
 
 
-def mem_usage_by_op(
-    graph: GraphLowering,
-    core_div_mismatch: dict[str, bool] = {},
-) -> dict:
+def mem_usage_by_op(graph: GraphLowering, ignore: list[str] = []) -> dict:
     """
     Get a summary of memory usage of the given operation. Two types of info can be found
     1. Name lists, e.g. mem_usage["all_inputs"], or "all_outputs", "all_buf_used"
@@ -46,97 +44,28 @@ def mem_usage_by_op(
     if a buf is not in core_div_mismatch => it has no users => graph output
     if a buf is on release_next => it's the last time it'll be used => allow inplace
     """
+    num_cores = get_ncores_for_buffers(graph)
     mem_usage: dict = {}
     for op in graph.operations:
         rw = op.get_read_writes()
-        mem_usage[op.name] = {
-            "all_inputs": [],
-            "all_outputs": [],
-        }
-        num_cores = math.prod(
-            [s for p in getattr(op, "op_it_space_splits", ()) for s in p.values()]
-        )
-
+        mem_usage[op.name] = {"all_inputs": []}
         for is_input, deps in [(True, rw.reads), (False, rw.writes)]:
             for dep in deps:
                 buf = graph.get_buffer(dep.name)
-                try:
-                    dev_layout = buf.layout.device_layout
-                    dev_size = (
-                        math.prod(dev_layout.device_size[:-1]) * 128
-                    )  # num_sticks * bytes_per_stick
-                    mem_usage[op.name][dep.name] = {
-                        "is_lx_viable": True,
-                        "is_input": is_input,
-                        "size": dev_size,
-                        "size_per_core": dev_size // num_cores,
-                        "core_div_mismatch": core_div_mismatch.get(dep.name, False),
-                    }
-                except AttributeError:
-                    mem_usage[op.name][dep.name] = {
-                        "is_lx_viable": False,
-                    }
+                dev_layout = buf.layout.device_layout
+                dev_size = (
+                    math.prod(dev_layout.device_size[:-1]) * 128
+                )  # num_sticks * bytes_per_stick
+                mem_usage[op.name][dep.name] = {
+                    "is_input": is_input,
+                    "size": dev_size,
+                    "size_per_core": dev_size // num_cores[op.name],
+                    "core_div_mismatch": num_cores[op.name] < 0,
+                }
 
                 if is_input:
                     mem_usage[op.name]["all_inputs"].append(dep.name)
-                else:
-                    mem_usage[op.name]["all_outputs"].append(dep.name)
-        mem_usage[op.name]["all_buf_used"] = (
-            mem_usage[op.name]["all_inputs"] + mem_usage[op.name]["all_outputs"]
-        )
     return mem_usage
-
-
-def buf_analysis(graph: GraphLowering):
-    """
-    First, find out the last time each buffer was used. {buf1: idx_last_used, ...}
-    Turn it into {idx_last_used+1:[buf1, ], ...}, ie. buffers to be deleted at given idx
-    Then check work division -> If any of the operations on a given buffer has different
-    work division => should not pin this buffer to LX
-    NOTE Because each core can only write to its own scratchpad. For example, if a
-            buffer is sliced 8 ways (stored on 8 LX) but next Op is 4-cores -> each core
-            in next op has to read from 2 different scratchpads...
-    TODO looking for options to broadcast to or all_reduce from multiple scratchpad
-    """
-    last_used: dict = {}
-    buf_read_counts: dict[str, int] = {}
-    buf_write_counts: dict[str, int] = {}
-    buf_users: dict[str, Operation] = {}
-    buf_users_read_and_write: dict[str, list[Operation]] = {}
-    core_div_mismatch: dict[str, bool] = {}
-
-    for idx, op in enumerate(graph.operations):
-        rw = op.get_read_writes()
-        read_names = op.get_read_names()
-        for dep in rw.reads | rw.writes:  # union of the OrderedSets
-            buf = dep.name  # buffer name, i.e. a str
-            last_used[buf] = idx
-            if buf in read_names:
-                buf_read_counts[buf] = buf_read_counts.get(buf, 0) + 1
-                buf_users[buf] = buf_users.get(buf, []) + [op]
-            else:
-                buf_write_counts[buf] = buf_write_counts.get(buf, 0) + 1
-            buf_users_read_and_write[buf] = buf_users_read_and_write.get(buf, []) + [op]
-
-    bufs_to_dealloc_at_idx: dict = {}
-    for buf, idx in last_used.items():
-        # if last used at idx => del at idx+1
-        if idx + 1 in bufs_to_dealloc_at_idx:
-            bufs_to_dealloc_at_idx[idx + 1].append(buf)
-        else:
-            bufs_to_dealloc_at_idx[idx + 1] = [buf]
-
-    using_multicore = config.sencores > 1
-    for buf_name, users_rw in buf_users_read_and_write.items():
-        # this dict includes graph input and output
-        same_core_div = True
-        if using_multicore and len(users_rw) > 1:
-            # graph input and output can have only 1 read or 1 write user.
-            u0_split = users_rw[0].op_it_space_splits  # a list like [16, 1]
-            same_core_div = all(u0_split == u.op_it_space_splits for u in users_rw[1:])
-        core_div_mismatch[buf_name] = not same_core_div
-
-    return bufs_to_dealloc_at_idx, buf_users, core_div_mismatch
 
 
 def get_buffer_users(graph: GraphLowering) -> dict[str, list[Operation]]:
@@ -147,3 +76,39 @@ def get_buffer_users(graph: GraphLowering) -> dict[str, list[Operation]]:
             buf = dep.name  # buffer name, i.e. a str
             buf_users_read_and_write[buf] = buf_users_read_and_write.get(buf, []) + [op]
     return buf_users_read_and_write
+
+
+def get_ncores_for_buffers(graph: GraphLowering) -> dict[str, int]:
+    """
+    Return a dictionary mapping buffer names to the number of cores
+    used by all the operations that uses the buffer.
+    If there is a core division mismatch return -1 instead of the
+    number of cores.
+    """
+    result: dict[str, int] = {}
+    using_multicore = config.sencores > 1
+    buf_users_read_and_write = get_buffer_users(graph)
+    for buf_name, users_rw in buf_users_read_and_write.items():
+        # this dict includes graph input and output
+        if using_multicore:
+            # graph input and output can have only 1 read or 1 write user.
+            u0_split = users_rw[0].op_it_space_splits  # a list like [16, 1]
+            same_core_div = all(u0_split == u.op_it_space_splits for u in users_rw[1:])
+            num_cores = (
+                math.prod([s for p in u0_split for s in p.values()])
+                if same_core_div
+                else -1
+            )
+        else:
+            num_cores = 1
+        result[buf_name] = num_cores
+    return result
+
+class GraphView(GraphLowering):
+    def __init__(
+        self, graph: GraphLowering, predicate: Callable[[GraphLowering], Operation]
+    ):
+        object.__init__(self)
+        self.__dict__.update(graph.__dict__)
+        self.graph = graph
+        self.operations = predicate(graph)
