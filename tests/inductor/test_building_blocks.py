d4a5d4bc511e4f53ca78cf7a3a3720d953dce7a4
Drew Hubley <dahubley@hotmail.ca>
2026-09-08 12:40:32 +0000

    Formatting
    Signed-off-by: Drew Hubley <dahubley@hotmail.ca>

--- tests/inductor/test_building_blocks.py
+++ tests/inductor/test_building_blocks.py
@@ -548,7 +548,7 @@ class TestBuildingBlocks(unittest.TestCase):
             transposed_inputs=True,
             reshape_output=True,
         )
-        
+
     @config.patch(
         {
             "cpsat_time_limit_seconds": 30,
