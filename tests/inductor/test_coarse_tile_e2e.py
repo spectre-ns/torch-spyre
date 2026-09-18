72ddf420cc81dc3d517f2f1c4e5b0e3af7b6d320
Drew Hubley <dahubley@hotmail.ca>
2026-09-18 18:58:29 +0000

    Patch solve time for CI
    Signed-off-by: Drew Hubley <dahubley@hotmail.ca>

--- tests/inductor/test_coarse_tile_e2e.py
+++ tests/inductor/test_coarse_tile_e2e.py
@@ -2975,6 +2975,7 @@ def test_flash_tile_Lk():
         )
 
 
+@config.patch({"cpsat_time_limit_seconds": 30})
 def test_flash_tile_B_H():
     """Flash v1: tile B÷2 H÷4. B=2."""
     run_coarse_tile_test(
