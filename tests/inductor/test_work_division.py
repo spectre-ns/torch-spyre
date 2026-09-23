3b2055ae784eff0e9c5488ea22329054b9f2f23a
Drew Hubley <dahubley@hotmail.ca>
2026-09-23 21:44:09 +0000

    revert minor changes
    Signed-off-by: Drew Hubley <dahubley@hotmail.ca>

--- tests/inductor/test_work_division.py
+++ tests/inductor/test_work_division.py
@@ -1448,6 +1448,24 @@ class TestFinalMappingConstraints(unittest.TestCase):
 
         self.assertEqual(result.blocked, {ki})
 
+    def test_depthwise_conv_does_not_block_trailing_group_dim(self):
+        kh, kw, group = (_isym(name) for name in ("kh", "kw", "group"))
+        op = _computed_buffer(
+            (8,),
+            name="depthwise_conv",
+            reduction_type=DEPTHWISE_CONV2D_OP,
+            reduction_ranges=(3, 3, 4),
+        )
+        result = reduction_window_blocked_vars(
+            _make_context(
+                op,
+                self._PLACEHOLDER_TD,
+                reduction_vars=[kh, kw, group],
+            )
+        )
+
+        self.assertEqual(result.blocked, {kh, kw})
+
     def test_conv_spatial_and_window_blocks_compose(self):
         mb, out, i, j, channel, ki = (
             _isym(name) for name in ("mb", "out", "i", "j", "channel", "ki")
